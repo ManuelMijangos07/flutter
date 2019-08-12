a9396e0b5b7f339e1b7a0396b690c5f7f9aa5066
Manuel Mijangos <mmijangos.c@outlook.com>
2019-08-12 10:33:36 -0600

    login screen

--- chat_app/lib/screens/welcome_screen.dart
+++ chat_app/lib/screens/welcome_screen.dart
@@ -7,10 +7,57 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  Widget getButton(MaterialAccentColor color, String name, VoidCallback onPressed){
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30),
+        elevation: 5.0,
+        child: SizedBox(
+          height: 43.0,
+          child: FlatButton(
+          child: Text(name),
+          onPressed: onPressed,
+        ),
+        )
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text(''),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Image.asset('images/logo.png'),
+                Text('Flutter Chat',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 48.0),
+            getButton(
+              Colors.lightBlueAccent,
+              'Log in',
+              () {}
+            ),
+            getButton(
+              Colors.blueAccent,
+              'Sing up',
+              () {}
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
